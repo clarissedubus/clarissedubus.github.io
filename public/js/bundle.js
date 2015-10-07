33fee0e854eb3c841074bd96ec1c61d5fa6b789f
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-10-07 17:22:40 +0100

    Build new version.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -10,8 +10,67 @@ var gaJsHost = "https:" == document.location.protocol ? "https://ssl." : "http:/
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
 try {
-  var pageTracker = _gat._getTracker("UA-XXXXXXXX-X");
-  pageTracker._trackPageview();
+    var pageTracker = _gat._getTracker("UA-XXXXXXXX-X");
+    pageTracker._trackPageview();
 } catch (err) {}
+
+function centerVerticallyInElement($element, $parent, $space) {
+    var ph = $parent.height(),
+        eoh = $element.outerHeight();
+
+    $space.css({
+        'height': (ph - eoh) / 2 + 'px'
+    });
+}
+function setMainContentHeight() {
+    var $w = $(window),
+        hoh = $('header').outerHeight(),
+        fph = $('footer').outerHeight(),
+        wh = $w.height(),
+        noh = $w.width() > 991 ? 0 : $('#nav-col').outerHeight();
+    $('#main-section').css({
+        'height': wh - hoh - fph - noh + 'px'
+    });
+}
+
+function reset() {
+    var $nav = $('#nav'),
+        $navParent = $('#nav-parent'),
+        $navSpace = $('#nav-space');
+
+    $nav.css({
+        'height': 'auto'
+    });
+    $navParent.css({
+        'height': 'auto'
+    });
+    $navSpace.css({
+        'height': 0
+    });
+}
+
+function render() {
+    var $nav = $('#nav'),
+        $navParent = $('#nav-parent'),
+        $navSpace = $('#nav-space');
+    var $mainContent = $('#main-content'),
+        $main = $('main'),
+        $mainSpace = $('#main-space');
+    setMainContentHeight();
+    if ($(window).width() > 991) {
+        centerVerticallyInElement($nav, $navParent, $navSpace);
+        centerVerticallyInElement($mainContent, $main, $mainSpace);
+    } else {
+        reset();
+    }
+}
+
+$(document).ready(function () {
+    render();
+});
+
+$(window).resize(function () {
+    render();
+});
 
 },{}]},{},[1]);
