cb35b8cd6b0881f033d8040e93092e9b486b677a
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-10-13 19:06:05 +0100

    Build latest version.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,5 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
+ * File:    app.js
  * Author:  @juancarlosfarah
  * Date:    05/10/15
  */
@@ -67,10 +68,22 @@ $(document).ready(function () {
     render();
     var $grid = $('.grid');
     imagesLoaded($grid, function () {
+
+        // Initialise Magnific Popup
+        $('.gallery').magnificPopup({
+            delegate: 'a',
+            gallery: {
+                enabled: true
+            },
+            type: 'image'
+        });
+
+        // Initialise Masonry
         $grid.masonry({
 
             // set itemSelector so .grid-sizer is not used in layout
             itemSelector: '.grid-item',
+            stamp: '.stamp',
 
             // use element for option
             columnWidth: '.grid-sizer',
@@ -83,13 +96,19 @@ $(document).ready(function () {
             "opacity": 1
         }, "slow");
 
-        $('.grid-item').hover(function () {
-            $(this).children('.project-hover').css({
-                visibility: 'visible'
+        // Bind event handlers to each grid item.
+        $('.grid-item.grid-img').each(function (i) {
+            $(this).hover(function () {
+                $(this).children('.project-hover').css({
+                    visibility: 'visible'
+                });
+            }, function () {
+                $(this).children('.project-hover').css({
+                    visibility: 'hidden'
+                });
             });
-        }, function () {
-            $(this).children('.project-hover').css({
-                visibility: 'hidden'
+            $(this).click(function () {
+                $('.gallery').magnificPopup('open', i);
             });
         });
     });
