43fcb599563e47b8cf4a01a793939bc3a0212cee
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-10-13 19:03:46 +0100

    Add magnific popup.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,6 +23,7 @@ gulp.task('vendor', function() {
     return gulp.src([
         'bower_components/jquery/dist/jquery.js',
         'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
         'bower_components/masonry/dist/masonry.pkgd.min.js',
         'bower_components/imagesloaded/imagesloaded.pkgd.min.js'
     ]).pipe(concat('vendor.js'))
